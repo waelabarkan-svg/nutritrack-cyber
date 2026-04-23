899b24433a5cd8cbf26ba0d5bac2211f14dd9550
Waël <wael.abarkan@gmail.com>
2026-04-23 12:18:05 +0000

    J'ai une erreur de Hydration Mismatch sur le RootLayout. Pour corriger ç

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="fr" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
