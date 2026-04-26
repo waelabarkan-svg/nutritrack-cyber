26b18321d80ac5c130a16a8a3f2c1812c0b41412
Waël <wael.abarkan@gmail.com>
2026-04-26 15:57:49 +0000

    Nettoyage de l'espace : Supprime manuellement le dossier out dans ton ex

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Le mode 'export' est désactivé pour permettre les Server Actions et le rendu dynamique
   eslint: {
     ignoreDuringBuilds: true,
   },
